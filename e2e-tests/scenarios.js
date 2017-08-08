471ba63cde8bdc86d595ace7bd1e008121b313ab
Abhishek Kumar <abhisheksliam@gmail.com>
2017-08-08 19:57:59 +0530

    temp updates

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,37 +5,37 @@
 describe('App', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /login when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getCurrentUrl()).toMatch("/view1");
+    expect(browser.getCurrentUrl()).toMatch("/login");
   });
 
 
-  describe('view1', function() {
+  describe('login', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view1');
+      browser.get('index.html#!/login');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render login when user navigates to /login', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch(/partial for login/);
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('dashboard', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view2');
+      browser.get('index.html#!/dashboard');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
+    it('should render dashboard when user navigates to /dashboard', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+        toMatch(/partial for dashboard/);
     });
 
   });
