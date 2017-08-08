dd376c5abd3403c134136fc2208877b08fa13fd4
Abhishek Kumar <abhisheksliam@gmail.com>
2017-08-08 13:05:37 +0530

    first commit

---
+++ karma.conf.js
@@ -0,0 +1,41 @@
+//jshint strict: false
+module.exports = function (config) {
+  config.set({
+
+    basePath: './app',
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'components/**/*.js',
+      'view*/**/*.js'
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter',
+      "karma-html-reporter"
+    ],
+
+    reporters: ['html', 'junit', 'progress'],
+
+    junitReporter: {
+      outputDir: 'unit_test_output',
+      outputFile: 'unit.xml',
+      suite: 'unit'
+    },
+
+    htmlReporter: {
+      outputDir: 'app/unit_test_output'
+    }
+
+  });
+};
